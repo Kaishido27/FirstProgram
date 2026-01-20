cbfe276726ac303293a6b8b97627286325500ead
Jairus Tanaka Morada <j.morada.533450@umindanao.edu.ph>
2026-01-20 09:26:55 +0800

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Id { get; set; } = 533450;
+
+        public string? Name { get; set; } = "Jairus T Morada";
+    }
+
+}
